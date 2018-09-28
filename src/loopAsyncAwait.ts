ff8d9f209397ae5d276904e255b80bcd7a5ba4de
Teddy Chambard <t.chambard@bee-buzziness>
2018-09-28 16:10:03 +0200

    loop samples with funnel/semaphore

--- src/loopAsyncAwait.ts
+++ src/loopAsyncAwait.ts
@@ -1,5 +1,6 @@
 import * as _ from 'lodash';
 import * as bluebird from 'bluebird';
+import Semaphore from 'semaphore-async-await';
 
 const durationTable = [
     500, 500, 500, 500,
@@ -7,7 +8,7 @@ const durationTable = [
 
 async function sequentialLoop() {
     console.log('> Sequential loop:');
-    console.log(`waiting for about ${_.sum(durationTable)} ms because of sequential loop`);
+    console.log(`waiting for about ${_.sum(durationTable)} ms`);
 
     const startDate = Date.now();
     await bluebird.each(durationTable, async (duration) => {
@@ -17,9 +18,21 @@ async function sequentialLoop() {
     console.log('total time: ', Date.now() - startDate);
 }
 
+async function sequentialLoopNotWorking() {
+    console.log('> [FAIL] Sequential loop with simple map:');
+    console.log(`waiting for about ${_.sum(durationTable)} ms`);
+
+    const startDate = Date.now();
+    await durationTable.map(async (duration) => {
+        await bluebird.delay(duration);
+    });
+
+    console.log('total time: ', Date.now() - startDate);
+}
+
 async function concurrentLoop() {
     console.log('> Concurrent loop:');
-    console.log(`waiting for about ${_.max(durationTable)} ms because of concurrent loop`);
+    console.log(`waiting for about ${_.max(durationTable)} ms`);
 
     const startDate = Date.now();
     await bluebird.map(durationTable, async (duration) => {
@@ -29,9 +42,64 @@ async function concurrentLoop() {
     console.log('total time: ', Date.now() - startDate);
 }
 
+async function concurrentLoopWithLocalSem() {
+    console.log('> Concurrent loop with sem(2) (with bluebird):');
+    console.log(`waiting for about ${_.sum(durationTable)/2} ms`);
+
+    const startDate = Date.now();
+
+    async function functionWithTwoConcurrentRunnersMax(duration) {
+        await bluebird.delay(duration);
+    }
+    await bluebird.map(durationTable, functionWithTwoConcurrentRunnersMax, { concurrency: 2 });
+
+    console.log('total time: ', Date.now() - startDate);
+}
+
+async function concurrentLoopWithGlobalSemNotWorking() {
+    console.log('> [FAIL] Concurrent loop with global sem(2) is called by 2 concurrent consumers (with bluebird):');
+    console.log(`waiting for about ${_.sum(durationTable)} ms`);
+
+    const startDate = Date.now();
+
+    async function functionWithTwoConcurrentRunnersMax(duration) {
+        await bluebird.delay(duration);
+    }
+
+    await bluebird.map([1, 2], async () => {
+        await bluebird.map(durationTable, functionWithTwoConcurrentRunnersMax, { concurrency: 2 });
+    });
+
+    console.log('total time: ', Date.now() - startDate);
+}
+
+async function concurrentLoopWithGlobalSemWorking() {
+    console.log('> Concurrent loop with global sem(2) is called by 2 concurrent consumers (with Semaphore):');
+    console.log(`waiting for about ${_.sum(durationTable)} ms`);
+
+    const startDate = Date.now();
+    const lock = new Semaphore(2);
+
+    async function loopFunction(duration) {
+        await lock.acquire();
+        await bluebird.delay(duration);
+        lock.release();
+    }
+
+    await bluebird.map([1, 2], async () => {
+        await bluebird.map(durationTable, loopFunction);
+    });
+
+    console.log('total time: ', Date.now() - startDate);
+}
+
 (async function () {
     await sequentialLoop();
     await concurrentLoop();
+    await sequentialLoopNotWorking();
+    await concurrentLoopWithLocalSem();
+    await concurrentLoopWithGlobalSemNotWorking();
+    await concurrentLoopWithGlobalSemWorking();
 }()).catch((e) => {
     console.error(e.stack);
 });
--- src/loopFPromise.ts
+++ src/loopFPromise.ts
@@ -1,4 +1,4 @@
-import { wait, run, map } from 'f-promise';
+import { wait, run, map, funnel } from 'f-promise';
 import * as _ from 'lodash';
 
 const durationTable = [
@@ -7,10 +7,10 @@ const durationTable = [
 
 function sequentialLoop() {
     console.log('> Sequential loop:');
-    console.log(`waiting for about ${_.sum(durationTable)} ms because of sequential loop`);
+    console.log(`waiting for about ${_.sum(durationTable)} ms`);
 
     const startDate = Date.now();
-    _.each(durationTable, (duration) => {
+    durationTable.forEach((duration) => {
         wait(cb => setTimeout(cb, duration));
     });
 
@@ -19,7 +19,7 @@ function sequentialLoop() {
 
 function concurrentLoop() {
     console.log('> Concurrent loop:');
-    console.log(`waiting for about ${_.max(durationTable)} ms because of concurrent loop`);
+    console.log(`waiting for about ${_.max(durationTable)} ms`);
 
     const startDate = Date.now();
     map(durationTable, (duration) => {
@@ -29,9 +29,48 @@ function concurrentLoop() {
     console.log('total time: ', Date.now() - startDate);
 }
 
+function concurrentLoopWithLocalFunnel() {
+    console.log('> Concurrent loop with funnel(2):');
+    console.log(`waiting for about ${_.sum(durationTable)/2} ms`);
+
+    const localFunnel = funnel(2);
+    const startDate = Date.now();
+
+    function loopFunction(duration) {
+        localFunnel(() => {
+            wait(cb => setTimeout(cb, duration));
+        })
+    }
+    map(durationTable, loopFunction);
+
+    console.log('total time: ', Date.now() - startDate);
+}
+
+function concurrentLoopWithGlobalFunnel() {
+    console.log('> Concurrent loop with global funnel(2) is called by 2 concurrent consumers:');
+    console.log(`waiting for about ${_.sum(durationTable)} ms`);
+
+    const startDate = Date.now();
+    const globalFunnel = funnel(2);
+
+    function functionWithTwoConcurrentRunnersMax(duration) {
+        globalFunnel(() => {
+            wait(cb => setTimeout(cb, duration));
+        })
+    }
+
+    map([1, 2], () => {
+        map(durationTable, functionWithTwoConcurrentRunnersMax);
+    });
+
+    console.log('total time: ', Date.now() - startDate);
+}
+
 run(() => {
     sequentialLoop();
     concurrentLoop();
+    concurrentLoopWithLocalFunnel();
+    concurrentLoopWithGlobalFunnel();
 }).catch((e) => {
     console.error(e.stack);
 });
